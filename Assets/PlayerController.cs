b3d397eb4e8556a1de0f3e6ac478ca69bab3f11f
Matthew Breitling <mattbreitling@tamu.edu>
2018-10-24 06:00:48 -0500

    initial commit

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public GameObject battleController, dialogueController, menuController, cam;
+
+    public float speed;
+
+    [HideInInspector]
+    private bool inMenu;
+
+    [HideInInspector]
+    private Vector3 cameraPos;
+
+    // Use this for initialization
+    void Start () {
+        inMenu = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        cameraPos = new Vector3(transform.position.x, transform.position.y, -5);
+        cam.transform.position = cameraPos;
+
+        if (!inMenu)
+        {
+
+            Vector2 movement = new Vector2(Input.GetAxis("Horizontal") * speed, Input.GetAxis("Vertical") * speed);
+            transform.GetComponent<Rigidbody2D>().AddForce(movement);
+        }
+	}
+}
---
+++ Assets/WorldController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldController : MonoBehaviour {
+
+    public GameObject battleController;
+    public GameObject dialogueController;
+    public GameObject menuController;
+    public GameObject playerController;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
