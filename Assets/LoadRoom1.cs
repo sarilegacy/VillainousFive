c784580b99f63bb3ed5759dd4e2295b337ef76b0
dylan-barnes65 <dylan.barnes65@tamu.edu>
2018-11-06 21:25:43 -0600

    Add files via upload

---
+++ Assets/LoadRoom1.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadRoom1 : MonoBehaviour
+{
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        // Only specifying the sceneName or sceneBuildIndex will load the Scene with the Single mode
+        {
+            SceneManager.LoadScene("Room2");
+        }
+    }
+}
