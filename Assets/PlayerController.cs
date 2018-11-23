c71ba081a085d8e2bd3a59611cfb4857cdca0ed5
Hamid Badamgul <hbadamgul21@linux2.cs.tamu.edu>
2018-11-22 21:26:03 -0600

    Fixed some issues, please git pull the changes. Don't Disregard them.

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -11,7 +11,6 @@ public class PlayerController : MonoBehaviour
 
     public float speed;
 	public Vector2 lastMove;
-
     private Animator anim;
 
     [HideInInspector]
@@ -46,29 +45,31 @@ public class PlayerController : MonoBehaviour
         cameraPos = new Vector3(transform.position.x, transform.position.y, -5);
         cam.transform.position = cameraPos;
 
-		Vector2 movement = new Vector2 (Input.GetAxis ("Horizontal") * speed, Input.GetAxis ("Vertical") * speed);
+		Vector2 movement = new Vector2 (Input.GetAxisRaw ("Horizontal") * speed, Input.GetAxisRaw ("Vertical") * speed);
+		anim.SetBool ("IsPlayerMoving", false);
 
-        if (dman.canmove == false && dman2.canmove == false && dman3.canmove == false && dman4.canmove == false)
-        {
-            if (!inMenu)
-            {
+		if (dman.canmove == false && dman2.canmove == false && dman3.canmove == false && dman4.canmove == false) 
+		{
+			if (!inMenu)
+			{
 				if (movement != Vector2.zero) 
 				{
 					anim.SetBool ("IsPlayerMoving", true);
 					anim.SetFloat ("MoveX", movement.x);
 					anim.SetFloat ("MoveY", movement.y);
-				
-					lastMove = new Vector2 (Input.GetAxis ("Horizontal") * speed, Input.GetAxis ("Vertical") * speed);
-				 
+
+					lastMove = movement;
+			 
 				} else
 					anim.SetBool ("IsPlayerMoving", false);
-              		transform.GetComponent<Rigidbody2D>().AddForce(movement);
-
-					anim.SetFloat ("LastMoveX", lastMove.x);
-					anim.SetFloat ("LastMoveY", lastMove.y);
-            }
-        }
 			
+				transform.GetComponent<Rigidbody2D> ().AddForce (movement);
+
+				anim.SetFloat ("LastMoveX", lastMove.x);
+				anim.SetFloat ("LastMoveY", lastMove.y);
+			}
+		}
+		
 
     }
 
