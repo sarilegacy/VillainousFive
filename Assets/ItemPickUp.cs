ce9692d7993346b9c8d1c9046615ccdb9a4df73d
Matthew Breitling <mattbreitling@tamu.edu>
2018-11-25 23:21:43 -0600

    Inventory updates

---
+++ Assets/ItemPickUp.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemPickUp : MonoBehaviour {
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.GetComponent<PlayerController>() == null)
+            return;
+
+        transform.SetParent(other.transform.GetChild(1));
+    }
+}
